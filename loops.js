fbc9de067662f4c6e03d5bea83f1e40ce693e2d6
Jason Viger <jason.viger@gmail.com>
2018-07-09 13:29:48 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -21,8 +21,12 @@ var whileLoop = function(number){
   return 'done';
 }
 
+function maybeTrue() {
+  return Math.random() >= 0.5 // Returns a random number between 0 (inclusive) and 1 (exclusive)
+}
 var doWhileLoop = function(array){
   
-  
+  do{}
+  while()
   
 }
