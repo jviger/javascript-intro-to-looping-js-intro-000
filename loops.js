1a19b04d8aa90c49ebe4e0f17af1ca791873602b
Jason Viger <jason.viger@gmail.com>
2018-07-09 13:16:18 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,7 +1,12 @@
 var forLoop = function(array){
   
   for(var i = 0; i<25; i++){
-    
+    if(i === 1){
+      array.push['I am 1 strange loop.']
+    }
+    else array.push["I am ${i} strange loops."]
   }
+  
+  return array;
   
 }
